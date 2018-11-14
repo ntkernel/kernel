2aae13bebe01c5d2af4c8e1f702486c5f97e31e0
ntkernel <1564940789@qq.com>
2018-11-14 17:49:47 +0800

    Update download.py

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 import requests
-r=requests.get("https://gitee.com/ntkernel/irobotq/raw/master/py.py")
+r=requests.get("https://gitee.com/ntkernel/irobotq/raw/master/main.py")
 print(r.content.decode())
 input()
