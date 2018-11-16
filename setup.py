bb57135d629bcd51fd7a2836a3ba14eac7ce5407
Microsoft <1564940789@qq.com>
2018-11-16 13:08:27 +0800

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("python -m pip install --upgrade pip")
-print("P")
-os.system("python -m pip install pyhooked >NUL")
-print("H")
-os.system("python -m pip install requests >NUL")
-print("R")
